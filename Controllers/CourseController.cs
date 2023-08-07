16a0d71913f9970a0b28623032c5e79109d2cc0e
seheerst <seherst9@gmail.com>
2023-08-07 17:57:41 +0300

    feat: created actionResults

--- Controllers/CourseController.cs
+++ Controllers/CourseController.cs
@@ -2,20 +2,31 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CourseCatalogApp.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CourseCatalogApp.Controllers
 {
     public class CourseController : Controller
     {
-        public string Index()
+        public IActionResult Index()
         {
-            return "Course/index";
+            var course = new Course();
+            course.Id = 1;
+            course.Title = "Asp.Net Core Kursu";
+            course.Description = "Bu kurs ile asp.net core ile uygulama geliştir";
+            course.Image = "img1.jpg";
+            return View(course);
         }
 
-        public string List()
+        public IActionResult List()
         {
-            return "Course/List";
+            var courses = new List<Course>(){
+               new Course{Id=1, Title="Asp.net Core Kursu", Description="Bu kurs ile asp.net core ile uygulama geliştir",Image= "img1.jpg"},
+               new Course{Id=2, Title="C# Kursu", Description="Bu kurs ile C# öğren",Image= "img_2.jpg"},
+               new Course{Id=3, Title="Flutter Kursu", Description="Flutter ile mobil uygulamalar geliştir",Image= "img2.jpg"},
+            };
+            return View(courses);
         }
     }
 }
